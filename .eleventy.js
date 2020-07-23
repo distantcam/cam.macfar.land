676af5f2c9d2ac2273d580013b65131e23354963
Cameron MacFarland <distantcam@gmail.com>
2020-07-23 13:47:47 +0800

    Added navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,23 @@
+const simpleIcons = require("simple-icons");
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.setUseGitIgnore(false);
+
 	eleventyConfig.addPassthroughCopy({ "_tmp/*.css": "." });
+
+	eleventyConfig.addShortcode("simpleicon", simpleIcon);
 };
+
+function simpleIcon(id) {
+	const data = simpleIcons.get(id);
+
+	if (!data) {
+		return `<p>SimpleIcon ID not found: '${id}'</p>`;
+	}
+
+	return `<div class="icon">
+	<svg width="24" height="24" data-icon="${id}" viewbox="0 0 24 24">
+		<path fill="currentColor" d="${data.path}" />
+	</svg>
+</div>`;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
 		content: ["./**/*.njk"],
 	},
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				main: "var(--color-main)",
+				index: "var(--color-index)",
+			},
+			spacing: {
+				"128": "32rem",
+			},
+			minHeight: (theme) => theme("spacing"),
+			maxWidth: {
+				"7xl": "80rem",
+			},
+		},
 	},
 	variants: {},
 	plugins: [],
