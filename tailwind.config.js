de3dec97c21c075246c0b3c5b1df780b66d8df61
Cameron MacFarland <distantcam@gmail.com>
2020-12-28 10:08:36 +0800

    Fixing purge settings for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const colors = require('tailwindcss/colors')
 module.exports = {
 	purge: {
 		content: [
-			'./_includes/**/*.njk',
-			'./index.njk'
+			'./site/_includes/**/*.njk',
+			'./site/index.njk'
 		],
 	},
 	darkMode: 'class', // false or 'media' or 'class'
