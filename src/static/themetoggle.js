b811892c0c6b9abcd94e81f230d4b03474eaf3d2
Cameron MacFarland <distantcam@gmail.com>
2020-12-28 16:16:17 +0800

    Added system preference to theming

--- src/static/themetoggle.js
+++ src/static/themetoggle.js
@@ -1,33 +1,53 @@
 window.addEventListener("DOMContentLoaded", function (event) {
-  const currentMode = localStorage.getItem("currentTheme") || "light";
-  updateTheme(currentMode);
+  updateTheme();
 });
 
 document.getElementById("toggle").addEventListener("click", function (event) {
   event.preventDefault();
-  const currentMode = localStorage.getItem("currentTheme") || "light";
-  const newMode = currentMode === "light" ? "dark" : "light";
-
-  localStorage.setItem("currentTheme", newMode);
 
   document.documentElement.classList.add("transition");
   setTimeout(() => {
     document.documentElement.classList.remove("transition");
   }, 1000);
 
-  updateTheme(newMode);
+  if (localStorage.theme === "light") {
+    localStorage.theme = "dark";
+  } else if (localStorage.theme === "dark") {
+    localStorage.removeItem("theme");
+  } else {
+    localStorage.theme = "light";
+  }
+
+  document.documentElement.classList.add("transition");
+  setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+
+  updateTheme();
 });
 
-function updateTheme(newMode) {
-  // document.documentElement.setAttribute("data-theme", newMode);
+function updateTheme() {
+  if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  const mode = localStorage.theme || "pref";
+
+  const pref = document.getElementById("pref");
   const moon = document.getElementById("moon");
   const sun = document.getElementById("sun");
-  if (newMode === "light") {
-    document.documentElement.classList.remove('dark');
+  if (mode === "pref") {
+    pref.classList.remove("hidden");
+    sun.classList.add("hidden");
+    moon.classList.add("hidden");
+  } else if (mode === "light") {
+    pref.classList.add("hidden");
     sun.classList.add("hidden");
     moon.classList.remove("hidden");
   } else {
-    document.documentElement.classList.add('dark');
+    pref.classList.add("hidden");
     sun.classList.remove("hidden");
     moon.classList.add("hidden");
   }
